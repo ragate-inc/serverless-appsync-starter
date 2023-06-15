2bee33d71af9fdba379be7593af22457f0698724
m-sugiyama-ragate <m.sugiyama@ragate.co.jp>
2023-06-15 21:01:29 +0900

    Input value from json to object

--- aws-node-appsync/src/middlewares/middy/errorCatch.ts
+++ aws-node-appsync/src/middlewares/middy/errorCatch.ts
@@ -1,3 +1,4 @@
+import { LogItemMessage } from '@aws-lambda-powertools/logger/lib/types';
 import middy from '@middy/core';
 import { AppSyncAuthorizerEvent, AppSyncResolverEvent, Context } from 'aws-lambda';
 import logger from 'utils/logger';
@@ -5,7 +6,7 @@ import logger from 'utils/logger';
 export default (): middy.MiddlewareObj<AppSyncAuthorizerEvent | AppSyncResolverEvent<unknown>, unknown, Error, Context> => {
   return {
     onError: (handler): void => {
-      logger.error(JSON.stringify(handler, null, 2));
+      logger.error(handler as unknown as LogItemMessage);
     },
   };
 };
